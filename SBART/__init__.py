b42cb611d0b72ac3d12ad236c000341028af946f
Kamuish <andremiguel952@gmail.com>
2022-05-03 12:40:27 +0100

    adding missing requires on the build system

--- SBART/__init__.py
+++ SBART/__init__.py
@@ -2,7 +2,7 @@
 SBART library.
 """
 
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 1, 4)
 __version__ = "-".join(map(str, __version_info__))
 
 # To avoid having supressed warnings during the SBART application
