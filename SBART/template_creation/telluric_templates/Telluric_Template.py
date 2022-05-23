075acec3ed1ae219d57fa9ce7a5e9c7f5668187f
Kamuish <andremiguel952@gmail.com>
2022-05-23 11:55:42 +0100

    fixing image dtype

--- SBART/template_creation/telluric_templates/Telluric_Template.py
+++ SBART/template_creation/telluric_templates/Telluric_Template.py
@@ -497,7 +497,7 @@ class TelluricTemplate(BaseTemplate):
         axis.plot(self.transmittance_wavelengths, self.transmittance_spectra)
         axis.set_xlabel(r"Wavelength [$\AA$]")
         axis.set_ylabel("Transmittance")
-        fig.savefig(metrics_path / f"transmittance_{self._associated_subInst}.fits")
+        fig.savefig(metrics_path / f"transmittance_{self._associated_subInst}.png")
         fig.close()
 
     def load_from_file(self, root_path: Path, loading_path: str) -> None:
