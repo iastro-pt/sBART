c0767e84e54272b09f7b78eb463b7ddb792b4a5b
Kamuish <andremiguel952@gmail.com>
2022-05-24 11:36:11 +0100

    chaning imports

--- SBART/Base_Models/__init__.py
+++ SBART/Base_Models/__init__.py
@@ -5,5 +5,4 @@ Base interfaces for the different SBART building blocks.
 
 from .BASE import BASE
 from .Frame import Frame
-from .Sampler_Model import SamplerModel
 from .UnitModel import UnitModel
