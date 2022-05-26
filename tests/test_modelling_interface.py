166cb24e3a1e2ee435c5d48ccd52e4974362cf5c
Kamuish <andremiguel952@gmail.com>
2022-05-26 17:09:14 +0100

    basic & manual tests of the modelling interface

---
+++ tests/test_modelling_interface.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+import numpy as np
+
+curr_folder = Path(__file__).absolute().parent
+
+from SBART.Instruments import ESPRESSO
+from SBART.outside_tools.create_logger import setup_SBART_logger
+from SBART.utils.shift_spectra import interpolate_data, apply_RVshift
+import matplotlib.pyplot as plt
+
+setup_SBART_logger(
+    "",
+    "",
+    instrument=ESPRESSO,
+    log_to_terminal=True,
+    write_to_file=False
+)
+
+in_data = ["/home/amiguel/seminar/data_files/ESPRESSO/proxima/0/r.ESPRE.2019-02-10T08:25:37.895_S2D_A.fits"]
+
+frames = []
+
+for kern in ["Matern-3_2", "Matern-5_2"]:
+    f_path = ESPRESSO(file_path=in_data[0],
+                      user_configs={"GP_KERNEL": kern}
+                      )
+
+    f_path.generate_root_path(curr_folder / "GP_fit")
+    frames.append(f_path)
+
+order = 100
+wave, flux, uncerts, mask = frames[0].get_data_from_spectral_order(order=order)
+
+# for frame in frames:
+#     frame.generate_model_from_order(order=order)
+#
+#     frame.trigger_data_storage()
+#
+#
+XX = apply_RVshift(wave[4000:4500], stellar_RV=100 / 1000)
+
+#
+# new_spec, new_errors, indexes = interpolate_data(original_lambda=wave,
+#                                                  original_spectrum=flux,
+#                                                  original_errors=uncerts,
+#                                                  new_lambda=XX,
+#                                                  propagate_interpol_errors="interpolation",
+#                                                  lower_limit=-np.inf,
+#                                                  upper_limit=np.inf
+#                                                  )
+
+fig, axis = plt.subplots(2, 1)
+
+colors = ["red", "green", "yellow"]
+
+for ind, mode in enumerate(["cubic", "quadratic", "pchip"]):
+    frames[0].set_interpolation_properties({"SPLINE_TYPE": mode})
+    new_spec, new_errs = frames[0].interpolate_spectrum_to_wavelength(order=order, new_wavelengths=XX, shift_RV_by=0, RV_shift_mode="apply")
+    axis[0].plot(XX, new_spec, color=colors[ind], linestyle="-.")
+
+
+# for f_index, frame in enumerate(frames):
+    # mean, mu = f_path.interpolate_spectrum_to_wavelength(new_wavelengths=XX, order=order)
+    # axis[0].plot(XX, mean, color=colors[f_index], linestyle="--")
+    # axis[0].fill_between(XX, mean - mu, mean + mu, color=colors[f_index], alpha=0.3)
+#     axis[1].scatter(XX, mean / new_spec, color=colors[f_index])
+#
+#
+# axis[0].fill_between(XX, new_spec - new_errors, new_spec + new_errors, color="blue", alpha=0.3)
+#
+inds = np.where(np.logical_and(wave >= XX[0], wave <= XX[-1]))
+axis[0].plot(wave[inds], flux[inds], color="black")
+plt.show()
