e64e90e27b72135834a2dfcce6faf6b9a409cced
Kamuish <andremiguel952@gmail.com>
2022-07-18 14:55:49 +0100

    adding a new continuum model

--- SBART/utils/RV_utilities/continuum_fit.py
+++ SBART/utils/RV_utilities/continuum_fit.py
@@ -1,17 +1,43 @@
+from functools import partial
+
 import numpy as np
 from loguru import logger
 from scipy import optimize
+from SBART.utils import custom_exceptions
 
 
-def chosen_trend(x, *poly_coeffs):
+def polynomial_continuum_model(x, model_coeffs):
     try:
-        return np.poly1d(poly_coeffs)(x)
+        return np.poly1d(model_coeffs)(x)
     except:
-        logger.warning("Problem in coeefs: {}".format(poly_coeffs))
+        logger.warning("Problem in coeefs: {}".format(model_coeffs))
 
 
-def fit_continuum_level(spectra_wave, spectra, template, interpolate_wave_indexes, fit_degree: int):
+def stretch_continuum_nodel(x, model_coeffs, template):
+    """
+    Implements the model specified by Xavier Dumusque:
+        A*Template + B*lambda + C
 
+    Parameters
+    =============
+    x:
+        Wavelength solution in which the model will be evaluated
+    model_coeffs:
+        Coefficients of the model, where the leftmost entry is A and all other entries are a polynomial
+    template:
+        The data that will be "scaled up"
+    """
+    return np.poly1d(model_coeffs[1:])(x) + template * model_coeffs[0]
+
+
+def fit_continuum_level(
+        spectra_wave,
+        spectra,
+        template,
+        interpolate_wave_indexes,
+        fit_degree: int,
+        continuum_type: str,
+):
     min_val = np.min(np.abs(template))
     offset = 1e6 if min_val < 1e-3 else 1
     spectral_ratio = (spectra * offset) / (template * offset)
@@ -21,6 +47,16 @@ def fit_continuum_level(spectra_wave, spectra, template, interpolate_wave_indexe
     xx_data = spectra_wave[interpolate_wave_indexes]
     yy_data = spectral_ratio
 
-    coefs, coefs_cov = optimize.curve_fit(chosen_trend, xx_data, yy_data, p0=p0)
-
-    return coefs, coefs_cov, spectral_ratio, chosen_trend
+    if continuum_type == "paper":
+        coeffs, coefs_cov = optimize.curve_fit(
+            polynomial_continuum_model, xx_data, yy_data, p0=p0
+        )
+        continuum_model = polynomial_continuum_model
+    elif continuum_type == "stretch":
+        Matrice1 = np.array([template, spectra_wave, np.ones(len(spectra_wave))]).transpose()
+        res1 = optimize.lsq_linear(Matrice1, xx_data, verbose=1)
+        coeffs, coefs_cov = res1.x, [0 for _ in res1.x]
+        continuum_model = partial(stretch_continuum_nodel, template=template)
+    else:
+        raise custom_exceptions.InternalError("Passed wrong keyword {} for the continuum type!", continuum_type)
+    return coeffs, coefs_cov, spectral_ratio, continuum_model
