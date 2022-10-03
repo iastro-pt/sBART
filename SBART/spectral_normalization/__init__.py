1fdb658d70612a7e4c94dcf8a1c866f457a56cc6
Kamuish <andremiguel952@gmail.com>
2022-10-03 14:25:31 +0100

    adding interface to run RASSINE

--- SBART/spectral_normalization/__init__.py
+++ SBART/spectral_normalization/__init__.py
@@ -5,9 +5,11 @@ to a new wavelength grid.
 """
 from .alpha_shape_normalization import AlphaShape_normalization
 from .polynomial_normalization import Polynomial_normalization
-from. derivative_normalization import Derivative_normalization
+from .derivative_normalization import Derivative_normalization
+from .RASSINE_normalization import RASSINE_normalization
 
 available_normalization_interfaces = {"Alpha-Shape": AlphaShape_normalization,
                                       "Poly-Norm": Polynomial_normalization,
-                                      "Derivative": Derivative_normalization
+                                      "Derivative": Derivative_normalization,
+                                      "RASSINE": RASSINE_normalization
                                       }
