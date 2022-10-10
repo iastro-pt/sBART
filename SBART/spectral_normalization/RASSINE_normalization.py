86355560fb72fbe5f280c337b4291fb19bdb91b1
Kamuish <andremiguel952@gmail.com>
2022-10-10 14:52:22 +0100

    ensureing that we use S1D

--- SBART/spectral_normalization/RASSINE_normalization.py
+++ SBART/spectral_normalization/RASSINE_normalization.py
@@ -34,6 +34,26 @@ class RASSINE_normalization(NormalizationBase):
         ...
         # TODO: check the commands to launch RASSINE
 
+        # reconstructed_S2D = np.zeros(s2d_sci.shape)
+        # reconstructed_wavelengths = np.zeros(s2d_sci.shape)
+        #
+        # order_number = 0
+        # order_size = 9111
+        # to_break = False
+        #
+        # while not to_break:
+        #     start_order = order_size * order_number
+        #     end_order = start_order + order_size
+        #     if end_order >= s1d_sol.size:
+        #         to_break = True
+        #         end_order = s1d_sol.size
+        #
+        #     slice_size = end_order - start_order
+        #     print(order_number)
+        #     reconstructed_wavelengths[order_number] = np.pad(s1d_sol[start_order:end_order], (0, order_size - slice_size))
+        #     reconstructed_S2D[order_number] = np.pad(s1d_sci[start_order:end_order], (0, order_size - slice_size))
+        #     order_number += 1
+
     def fit_normalization(self, wavelengths, flux, uncertainties):
         # This will do nothing, as the RASSINE FIT was computed at init-time
         # TODO: check the info to return in here!
@@ -48,4 +68,5 @@ class RASSINE_normalization(NormalizationBase):
     def _normalization_sanity_checks(self):
         # TODO: check this, maybe we will be limited to BLAZE-corrected spectra!
         logger.debug("{} does not apply any sanity check on the data!")
-        ...
+        if not self._spec_info["is_S1D"]:
+            raise custom_exceptions.InvalidConfiguration("RASSINE model is only applicable to S1D data!")
