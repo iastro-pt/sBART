a75547208d9dc956ce30021c40270eeee0ecba2f
Kamuish <andremiguel952@gmail.com>
2022-11-02 13:40:45 +0000

    fixing available dunits

--- SBART/DataUnits/__init__.py
+++ SBART/DataUnits/__init__.py
@@ -1,4 +1,4 @@
 from .ClassicalUnit import Classical_Unit
 from .SpecNormUnit import SpecNorm_Unit
 
-available_data_units = [Classical_Unit, SpecNormUnit]
+available_data_units = [Classical_Unit, SpecNorm_Unit]
