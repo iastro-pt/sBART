736f301b697b89e05367c410e9835f3f33da7ee2
Kamuish <andremiguel952@gmail.com>
2023-01-16 22:25:16 +0000

    adding tests to the DLW computation

--- SBART/utils/RV_utilities/compute_DLW.py
+++ SBART/utils/RV_utilities/compute_DLW.py
@@ -1,5 +1,6 @@
 import numpy as np
 from loguru import logger
+from matplotlib import pyplot as plt
 
 from SBART.utils.math_tools.numerical_derivatives import first_numerical_derivative
 import scipy.optimize as sc
@@ -7,28 +8,31 @@ import scipy.optimize as sc
 
 def compute_DLW(spec_wave, spec_flux, spec_variance, temp_flux, temp_variance):
     # Compute second derivation of the template
+
     derivative, errors = first_numerical_derivative(spec_wave, temp_flux, np.sqrt(temp_variance))
     derivative, errors = first_numerical_derivative(spec_wave, derivative, errors)
-    weights = 1 / (errors ** 2 + spec_variance)
+    derivative = np.asarray(derivative)
+    errors = np.asarray(errors)
+    weights = np.divide(1, errors ** 2 + spec_variance)
 
     squared_derivative = derivative ** 2
     squared_derivative_errors = errors ** 2
-    residuals = spec_flux - temp_flux
+    residuals = temp_flux - spec_flux
+
     squared_residuals = residuals ** 2
     squared_weights = weights ** 2
-    A = np.sum(weights * derivative * residuals)
-    B = np.sum(weights * squared_derivative)
+    A = np.dot(weights * derivative, residuals)
+    B = np.dot(weights, squared_derivative)
 
     res_variance = temp_variance + spec_variance
-    sigma_A = np.sum(squared_weights * squared_derivative * squared_residuals * (squared_derivative_errors/squared_derivative + res_variance/squared_residuals))
-    sigma_B = np.sum(4*squared_weights*squared_derivative_errors/squared_derivative)
+    sigma_A = np.sum(squared_weights * squared_derivative * squared_residuals * (
+                squared_derivative_errors / squared_derivative + res_variance / squared_residuals)
+                     )
+    sigma_B = np.sum(squared_weights * (2*derivative * errors)**2)
 
     dlw = A / B
-    cov_dlw = dlw**2 * (sigma_A / A**2 + sigma_B / B**2)
+    cov_dlw = dlw ** 2 * (sigma_A / A ** 2 + sigma_B / B ** 2)
 
-    # dlw, cov_dlw = sc.curve_fit(lambda x, param: np.multiply(param, derivative), spec_wave, spec_flux - temp_flux, p0=1,
-    #                             sigma=spec_variance + temp_variance
-    #                             )
     if cov_dlw < 0:
         logger.warning("Covariance of the DLW value is negative. Changing it for nan")
         err_dlw = np.nan
@@ -37,4 +41,39 @@ def compute_DLW(spec_wave, spec_flux, spec_variance, temp_flux, temp_variance):
     return dlw, err_dlw
 
 
+if __name__ == "__main__":
+    print("oasidoas")
 
+
+    def normal_f(x, mu, sigma):
+        '''Gaussiana clássica'''
+
+        dentro = (x - mu) / sigma
+        princip = np.exp(-0.5 * dentro ** 2)
+
+        return np.asarray((princip / (sigma * (2 * np.pi) ** 0.5)))
+
+
+    spec = np.linspace(-50, 50, 2000)
+    print(spec.size)
+
+    sigma = 10
+    d_sigma = 0.1
+    amp = -5000
+    fig, axis = plt.subplots()
+    for d_sigma in np.linspace(-0.5, 0.5, 50):
+        gauss = amp*normal_f(spec, 0, sigma)
+        gauss_mod = amp*normal_f(spec, 0, sigma + d_sigma)
+        dlw, err = compute_DLW(spec, spec_flux=gauss,
+                               spec_variance=(0.05 * spec) ** 2,
+                               temp_flux=gauss_mod,
+                               temp_variance=(0.01 * gauss_mod) ** 2
+                               )
+
+        injected = sigma * d_sigma
+
+        axis.errorbar(100*d_sigma / sigma, 100 * (dlw - injected) / injected, err, color="black", marker='x', ls='')
+    axis.set_ylabel("100*(DLW - injected)/injected")
+    axis.set_xlabel(r"100*$\Delta \sigma / \sigma$")
+
+    plt.show()
