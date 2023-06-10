63cff95ffdc4ce46068c2dbfb5996fb6e0c0ef51
Kamuish <andremiguel952@gmail.com>
2023-06-10 11:08:09 +0100

    ensure path for requirements
    (cherry picked from commit 0eaa29827b7837d33df28ef53d33eab7c78ca2a9)

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 from pathlib import Path
+
+import numpy
 
 import setuptools
 
@@ -8,21 +11,36 @@ from setuptools import Extension
 
 version = "0.4.0"
 
-
-USE_CYTHON = False   # command line option, try-import, ...
+USE_CYTHON = False  # command line option, try-import, ...
 
 ext = '.pyx' if USE_CYTHON else '.c'
 
 targ_path = curr_file / "SBART" / "utils" / "cython_codes"
 
-pyx_files = targ_path.glob(f"**/*{ext}")
-
+pyx_files = list(targ_path.glob(f"**/*{ext}"))
 targets = {}
 for entry in pyx_files:
     parts = entry.relative_to(curr_file).parts
     parts = parts[:-1] + (parts[-1].split(".")[0],)
+    targets[".".join(parts)] = (entry.relative_to(curr_file)).as_posix()
 
-    targets[".".join(parts)] = (entry.relative_to(curr_file )).as_posix()
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
 
 ext_modules = [
     Extension(
@@ -39,24 +57,37 @@ compiler_directives = {"language_level": 3, "embedsignature": True}
 if USE_CYTHON:
     from Cython.Build import cythonize
     import Cython.Compiler.Options
+
     Cython.Compiler.Options.annotate = True
     from Cython.Build import cythonize
 
     ext_modules = cythonize(ext_modules,
-                        compiler_directives=compiler_directives,
-                        )
+                            compiler_directives=compiler_directives,
+                            )
 
 from distutils.core import setup
 
-all_packages = setuptools.find_packages(where=".", include=["SBART", "SBART.*"])
-with open('requirements.txt') as f:
+all_packages = setuptools.find_namespace_packages("SBART")
+
+print(all_packages)
+
+requ_path = Path(__file__).parent
+with open(requ_path / 'requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(name='SBART',
+setup(name='SBART', package_dir={"": "SBART"},
       version=version,
       description='Python Distribution Utilities',
       packages=all_packages,
       include_package_data=True,
       ext_modules=ext_modules,
-      install_requires=required
+      install_requires=required,
+      include_dirs=[numpy.get_include()],
+      # package_data={"SBART": ["utils/tapas_downloader",
+      #                         "resources/*",
+      #                         "outside_tools/*",
+      #                         "utils/cython_codes/cubic_interpolation/__init__.py"
+      #                         ]
+      #               }
+
       )
